2459372cd53c92e99fd103ca2e5dee0fb1cacc09
dilli <dillihangrae@gmail.com>
2022-05-15 19:10:19 +0545

    [added] adds day 7 node js with huddle nepal

---
+++ Huddle-Nepal/Node-Js/Live-Training/May-13/day-6.js
@@ -0,0 +1,25 @@
+
+
+// API Testing Software
+/**
+ *  Postman, thunderClient(lightweight), Insomia
+ *  using extension 
+ */
+
+/**
+ * MiddleWares -- methods/functions/operations that are called Between processing and sending the response
+ * 
+ * Levels of MiddleWares
+ *   - application middlewares - deployed middleware on application itself
+ *   - router middleware - used on router/routing method with next() method
+ *   - inbuilt middleware - express serving the static files
+ *   - Third party middleware
+ *   - 
+ */
+
+/**
+ * package - express rate limit
+ *     - limit the api call rate like in 15 minutes api calling rate is only 100 times
+ * 
+ *  # helmet library for security purposes in backend and can be worked with middleware.
+ */
